5b58734706c5441dbfba5e50961b712ac8f195bb
maanhi1 <maanhiiiii@gmail.com>
2024-11-29 14:09:12 +0700

    them san pham - moi

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,80 +22,71 @@ const Login = () => {
     } else {
       alert("Email hoặc mật khẩu không đúng!");
     }
+  };
 
-    const Login = () => {
-      const [currentState, setCurrentState] = useState("Sign Up");
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800"></hr>
+      </div>
+      {currentState === "Login" ? (
+        " "
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
 
-      const onSubmitHandler = async (event) => {
-        event.preventDefault();
-      };
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
 
-      return (
-        <form
-          onSubmit={onSubmitHandler}
-          className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
-        >
-          <div className="inline-flex items-center gap-2 mb-2 mt-10">
-            <p className="prata-regular text-3xl">{currentState}</p>
-            <hr className="border-none h-[1.5px] w-8 bg-gray-800"></hr>
-          </div>
-          {currentState === "Login" ? (
-            " "
-          ) : (
-            <input
-              type="text"
-              className="w-full px-3 py-2 border border-gray-800"
-              placeholder="Name"
-              required
-            />
-          )}
-
-          <input
-            type="email"
-            className="w-full px-3 py-2 border border-gray-800"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-
-            required
-          />
-          <input
-            type="password"
-            className="w-full px-3 py-2 border border-gray-800"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-
-          <div className="w-full flex justify-between text-sm mt-[-8px]">
-            <p className="cursor-pointer">Forgot your passsword?</p>
-            {currentState === "Login" ? (
-              <p
-                onClick={() => setCurrentState("Sign Up")}
-                className="cursor-pointer"
-              >
-                Create account{" "}
-              </p>
-            ) : (
-              <p
-                onClick={() => setCurrentState("Login")}
-                className="cursor-pointer"
-              >
-                Login Here
-              </p>
-            )}
-          </div>
-          <button
-            type="submit"
-            className="bg-black text-white font-light px-8 py-2 mt-4"
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Forgot your passsword?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
           >
-            {/* <button className="bg-black text-white font-light px-8 py-2 mt-4"> */}
-            {currentState === "Login" ? "Sign In" : "Sign Up"}
-          </button>
-        </form>
-      );
-    };
-  }
-}
+            Create account{" "}
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="bg-black text-white font-light px-8 py-2 mt-4"
+      >
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
+};
+
 export default Login;
