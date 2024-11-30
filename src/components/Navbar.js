8fd19b8a9143a16ca7d9e1d284a42e715bddc16b
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-11-30 09:52:23 +0700

    sửa lại icon user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,30 +41,28 @@ const Navbar = () => {
             alt=""
           />
         </Link>
-         <img
+        <img
           onClick={() => setShowSearch(true)}
           src={assets.search_icon}
           className="w-5 cursor-pointer"
           alt=""
         />
 
-       
+
 
 
 
         <div className="group relative">
-          <Link to="/login">
-            <img
-              className=" w-5 cursor-pointer"
-              src={assets.profile_icon}
-              alt=""
-            />
-          </Link>
+
+          <img
+            className=" w-5 cursor-pointer"
+            src={assets.profile_icon}
+            alt=""
+          />
+
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded border hover:bg-white hover:border-black ">
+              <Link to="/login" className="cursor-pointer hover:text-black ">Login</Link>
             </div>
           </div>
         </div>
